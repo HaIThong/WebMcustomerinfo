3f1cafa22883858d09583158e6b6b59cc57fb78a
trucanh21 <taxuanlan2002@gmail.com>
2024-06-19 01:11:19 +0700

    add page customer managementment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import KhachHangIF from "./components/KhachHangIF"
+import Khachhang from "./components/Khachhang";
+import Sidebar from "./components/Sidebar";
+import MainPanel from "./components/MainPanel";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <KhachHangIF/>
+    // <div className="App">
+    //   <Khachhang/>
+    // </div>
+    <div className="wrapper">
+      <Sidebar />
+      <MainPanel />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
